14d4ae9b445ff627fd860984ce388a7d8a04de52
Hanjo Schumacher <dev@hanjo.dev>
2024-07-03 15:38:10 +0200

    init commit

---
+++ data/config.js
@@ -0,0 +1,7 @@
+// ------------------------------ START OF CONFIGURATION ------------------------------ //
+const enable24HR = false;
+const enableBacklight = false;
+//if you have your own dimensions, set it here. Otherwise, leave it blank for auto-configuration
+let HEIGHT = 0;
+let WIDTH = 0;
+// ------------------------------ END OF CONFIGURATION ------------------------------ //
